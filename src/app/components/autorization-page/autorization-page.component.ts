ed98439c540c29ee7d57dec30a3fb34006ee6faa
Olga <olgapasibayeva98@gmail.com>
2018-01-22 16:00:23 +0200

    autorization page version 0.0

---
+++ src/app/components/autorization-page/autorization-page.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: './components/autorization-page/autorization-page.component.html',
+  styleUrls: ['./components/autorization-page/autorization-page.component.css']
+
+})
+
+export class AutorizationPageComponent {
+  title = 'IdeaFix';
+}
