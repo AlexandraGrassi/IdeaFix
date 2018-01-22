89fcbe07230cb588338b3d72ae20799e332c12d9
Olga <olgapasibayeva98@gmail.com>
2018-01-22 15:59:07 +0200

    autorization page version 0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './components/autorization-page/autorization-page.component.html',
+  styleUrls: ['./components/autorization-page/autorization-page.component.css']
+})
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './components/registration-page/registration-page.component.html',
+  styleUrls: ['./components/registration-page/registration-page.component.css']
 })
 export class AppComponent {
   title = 'app works!';
