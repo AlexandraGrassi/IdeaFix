2ac716225e039dc1f1fe9e58f660cec9b6d6b692
Alexandra <alexandraaagrassi@gmail.com>
2018-01-23 22:17:49 +0200

    Add idea card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './components/autorization-page/autorization-page.component.html',
-  styleUrls: ['./components/autorization-page/autorization-page.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './components/registration-page/registration-page.component.html',
-  styleUrls: ['./components/registration-page/registration-page.component.css']
-})
 export class AppComponent {
   title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { IdeaCardComponent } from './idea-card/idea-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeaCardComponent
   ],
   imports: [
     BrowserModule,
