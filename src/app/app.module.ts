a9bca607b8e7d69ac6c86e0ef1ae077eef7f0a1e
Alexandra <alexandraaagrassi@gmail.com>
2018-01-24 16:55:38 +0200

    Add pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+/*import {HttpClientModule} from '@angular/common/http';*/
 
 import { AppComponent } from './app.component';
 import { IdeaCardComponent } from './components/idea-card/idea-card.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { IdeaPageComponent } from './components/idea-page/idea-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IdeaCardComponent
+    IdeaCardComponent,
+    NavBarComponent,
+    HomePageComponent,
+    IdeaPageComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    /*HttpClientModule*/
   ],
   providers: [],
   bootstrap: [AppComponent]
